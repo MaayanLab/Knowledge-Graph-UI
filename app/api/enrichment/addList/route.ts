8e60b23835d6a3a97065b592372c9eb095c7b99e
Anna Byrd <aibyrd19@gmail.com>
2024-11-26 13:30:32 -0500

    check if input set exists before assigning new id

--- app/api/enrichment/addList/route.ts
+++ app/api/enrichment/addList/route.ts
@@ -3,7 +3,7 @@ import { neo4jDriver } from "@/utils/neo4j"
 import { NextResponse } from "next/server"
 import { NextRequest } from 'next/server'
 import { z } from 'zod'
-import {v4 as uuidv4, v5 as uuidv5} from 'uuid'
+import {v4 as uuidv4} from 'uuid'
 
 interface GeneSet {
     list: string,
@@ -23,10 +23,19 @@ export async function POST(req: NextRequest) {
             const formData = await req.formData() // get gene set
             const list = formData.get('list') as string | null
             const description = formData.get('description') as string | null
-            const namespace = uuidv4() // create persistent ID
-            const userListId:string = uuidv5(list, namespace)
+            
+            const tryquery =  `MATCH (a:GeneSet) WHERE a.gene_set = "${list}" AND a.description = "${description}" RETURN a.id`
+            const test = await read_session.readTransaction(txc => txc.run(tryquery))
+            let userListId:string = uuidv4()
+            //console.log("record id: ", test.records)
+            if (test.records.length !== 0) {
+                //const namespace = uuidv4() // create persistent ID
+                userListId = test.records[0].get('a.id')
+                console.log("id: ", userListId)
+                //console.log("record: ", test.records)
+            }
             const query = `MATCH (a:GeneSet) WHERE a.id = "${userListId}" RETURN a.id`
-    
+            
             const rs = await read_session.readTransaction(txc => txc.run(query))
             if (rs.records.length === 0) {
                 const query = `CREATE (n:\`GeneSet\` {id: "${userListId}", gene_set: "${list}", description: "${description}"}) RETURN n.id` // add node to graph
